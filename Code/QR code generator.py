45768d66a171ac4652329726243895a89c006ca1
SamJebarajNadar <82108546+SamJebarajNadar@users.noreply.github.com>
2021-04-07 22:51:46 +0530

    Add files via upload

---
+++ Code/QR code generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import qrcode
+qr=qrcode.QRCode(version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=20,
+                border=2)
+qr.add_data('https://samsammy1.blogspot.com')
+qr.make(fit=True)
+
+img=qr.make_image(fill_color="black",back_color="white")
+img.save('Blog.png')
+
